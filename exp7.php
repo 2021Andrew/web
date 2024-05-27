487566877f8e568ba04656c3435894837ecbf9f0
2021Andrew <andrewphiri843@gmail.com>
2024-05-27 05:54:39 +0200

    first commit

---
+++ exp7.php
@@ -0,0 +1,20 @@
+<html>
+    <body> 
+    <?php 
+//date_default_timezone_set('Asia/Calcutta'); 
+ 
+//Calculate 60 days in the future 
+//seconds * minutes * hours * days + current time 
+ 
+$inTwoMonths = 60 * 60 * 24 * 60 + time();  
+ 
+setcookie( $inTwoMonths);  
+ 
+if(isset($_COOKIE['lastVisit'])) 
+{ 
+     $visit = $_COOKIE['lastVisit'];       echo "Your last visit was - ". $visit; 
+} else echo "You've got some stale cookies!"; 
+?> 
+
+    </body>
+</html>
